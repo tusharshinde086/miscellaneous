fee982500d2387d4d300e1d665ca184f3e653b07
Tushar Shinde <tusharshindets086@gmail.com>
2025-05-13 22:45:14 +0530

    http:// localhost : 8080/ register

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 8080;
 
 app.get("/register", (req , res )=>{
+    let {user , password } = req.query;
     // console.log("accepted ");
-    res.send("standard get responce");
+    res.send(`standard get responce . welcome ${user}!`);
 });
 
 app.post("/register", (req , res )=>{
