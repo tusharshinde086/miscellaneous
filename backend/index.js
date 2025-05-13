9e3a7fa8e089ee922e2edf1305f0df95ad3b0ca8
Tushar Shinde <tusharshindets086@gmail.com>
2025-05-13 22:34:52 +0530

    creating js backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.get("/register", (req , res )=>{
+    // console.log("accepted ");
+    res.send("standard get responce");
+});
+
+app.post("/register", (req , res )=>{
+    // console.log("accepted ");
+    res.send("standard post responce");
+});
+
 app.listen(port, () => {
     console.log(`listening to the port ${port}`);
 });
